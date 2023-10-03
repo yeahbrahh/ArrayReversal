61ef6f2d3d71f721fad6ca6504d47374d4e2f980
Gage <121586198+yeahbrahh@users.noreply.github.com>
2023-10-03 16:38:15 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int[] numberArray = new int[100];
-        int numbers = 0;
+        int[] numberArray = new int[100]; // create array with a maximum of 100 elements
+        int numbers = 0; // initialize numbers variable to 0
 
         System.out.println("Enter values (-1 to stop) ");
 
@@ -15,13 +14,18 @@ public class Main {
 
             int num = sc.nextInt();
 
+            //   break loop when user enters -1
             if (num == -1) {
                 break;
             }
+            //  num is values entered
+            // numbers is position of num values in array
+            // numberArray is the array itself
             numberArray[numbers] = num;
-            numbers++;
+            numbers++;  // increments numbers to next position in array
         }
-        // Prints numbers in reverse
+        // reassigns i variable to numbers - 1 to begin at end of index of array
+        // decrements i variable to move to previous position in array
         System.out.println("Your values, reversed: ");
         for (int i = numbers - 1; i >= 0; i--) {
             System.out.println(numberArray[i] + " ");
